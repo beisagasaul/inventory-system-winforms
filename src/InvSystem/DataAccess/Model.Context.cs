da0afc107ab38f3a88035849a7022f1e63b05f10
beisagasaul <147957141+beisagasaul@users.noreply.github.com>
2026-04-28 22:04:04 -0400

    modified model.tt

--- src/InvSystem/DataAccess/Model.Context.cs
+++ src/InvSystem/DataAccess/Model.Context.cs
@@ -12,11 +12,13 @@ namespace DataAccess
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Infrastructure;
+    using System.Data.Entity.Core.Objects;
+    using System.Linq;
     
-    public partial class inventory_dbEntities1 : DbContext
+    public partial class inventory_dbEntities2 : DbContext
     {
-        public inventory_dbEntities1()
-            : base("name=inventory_dbEntities1")
+        public inventory_dbEntities2()
+            : base("name=inventory_dbEntities2")
         {
         }
     
@@ -38,5 +40,19 @@ namespace DataAccess
         public virtual DbSet<sysdiagrams> sysdiagrams { get; set; }
         public virtual DbSet<UnitOfMeasure> UnitOfMeasure { get; set; }
         public virtual DbSet<UserAccount> UserAccount { get; set; }
+    
+        public virtual int sp_ProductSearch(string parametro)
+        {
+            var parametroParameter = parametro != null ?
+                new ObjectParameter("parametro", parametro) :
+                new ObjectParameter("parametro", typeof(string));
+    
+            return ((IObjectContextAdapter)this).ObjectContext.ExecuteFunction("sp_ProductSearch", parametroParameter);
+        }
+    
+        public virtual int sp_TestSimple()
+        {
+            return ((IObjectContextAdapter)this).ObjectContext.ExecuteFunction("sp_TestSimple");
+        }
     }
 }
